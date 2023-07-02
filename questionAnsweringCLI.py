fee7e60ac4dc60446ba192501ddfbcbb5a2104b6
GrzegorzDjango <48175719+GrzegorzDjango@users.noreply.github.com>
2023-07-02 05:29:39 +0000

    qac

---
+++ questionAnsweringCLI.py
@@ -0,0 +1,29 @@
+#!/user/bin/env python
+
+
+import openai
+import os
+import click
+
+def submit_question(text):
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    prompt = text
+    result = openai.Completion.create(
+        prompt=prompt,
+        temparature=0,
+        max_tokens=300,
+        top_p=1,
+        frequency_penalty=1,
+        presence_penalty=0,,
+        model="text-davinci-002"
+    )["choices"][0]["text"].strip("\n")
+    return result
+
+@click.command()
+@click.argument('text')
+def main(text):
+    print(submit_question(text))
+
+if __name__ == "__main__":
+    main()
