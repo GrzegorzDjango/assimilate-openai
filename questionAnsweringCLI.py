82cd80da6c66bea8eece8d6c2b5050ecf50d608f
GrzegorzDjango <48175719+GrzegorzDjango@users.noreply.github.com>
2023-07-02 05:37:39 +0000

    xx

--- questionAnsweringCLI.py
+++ questionAnsweringCLI.py
@@ -6,16 +6,18 @@ import os
 import click
 
 def submit_question(text):
-
+    print(os.getenv("OPENAI_API_KEY"))
+    key = "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"
     openai.api_key = os.getenv("OPENAI_API_KEY")
+
     prompt = text
     result = openai.Completion.create(
         prompt=prompt,
-        temparature=0,
+        temperature=0,
         max_tokens=300,
         top_p=1,
         frequency_penalty=1,
-        presence_penalty=0,,
+        presence_penalty=0,
         model="text-davinci-002"
     )["choices"][0]["text"].strip("\n")
     return result
